f599793251bd5aff0af5dd56227ce78963907742
Yuval Greenfield <ubershmekel@gmail.com>
2016-08-20 13:25:59 -0700

    Started woodbridge specific style Added google css as base

--- panel.js
+++ panel.js
@@ -1,7 +1,7 @@
 google.maps.event.addDomListener(window, 'load', function() {
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: new google.maps.LatLng(-28, 135),
-    zoom: 4,
+    center: new google.maps.LatLng(33.677,-117.797),
+    zoom: 15,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
